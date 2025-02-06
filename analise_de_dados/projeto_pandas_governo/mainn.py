1349fc687f98d02d806aaa380da0f60770276ea8
Joao araujo <joaoaraujo.vitor@hotmail.com>
2025-02-06 15:48:37 -0300

    tratando dados

--- analise_de_dados/projeto_pandas_governo/mainn.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd 
-
-dados_viagem = "/home/joao/Estudos_py_ml/analise_de_dados/projeto_pandas_governo/2023_Viagem.csv"
-
-df_viagens = pd.read_csv(dados_viagem, encoding="Windows-1252", sep=";")
-print(df_viagens)
-
